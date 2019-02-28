39f8afa3ea4cbdd5f542574eddeb315037373504
jeff15113 <jeff15113@users.noreply.github.com>
2019-02-28 13:42:39 -0500

    tokens index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 //const helmet = require('helmet');
 //const cors = require('cors');
 const bcrypt = require("bcrypt");
-
-const db = require("./dbConfig.js");
 const Users = require("./userdb.js");
 
 const server = express();
+
+// thats a good secret i guess
+const secret = process.env.JWT_SECRET || "this is good stuff";
 
 //server.use(helmet());
 server.use(express.json());
@@ -27,6 +28,20 @@ server.post("/api/register", (req, res) => {
     });
 });
 
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    roles: ["Admin"]
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
 server.post("/api/login", (req, res) => {
   let { username, password } = req.body;
 
@@ -34,7 +49,13 @@ server.post("/api/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Willkommen, ${user.username}!` });
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Willkommen, ${user.username}!, have a token...`,
+          token,
+          secret,
+          roles: token.roles
+        });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
@@ -45,13 +66,41 @@ server.post("/api/login", (req, res) => {
 });
 
 function restricted(req, res, next) {
-  next();
+  const token = req.headers.authorization;
+
+  if (token) {
+    // If we have a token
+    // verify its valid
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        // if error
+        // send
+        res.status(401).json({ you: "can't touch this!" });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: "shall not pass!" });
+  }
 }
 
-server.get("/api/users", restricted, (req, res) => {
+function checkRole(role) {
+  return function(req, res, next) {
+    // check decoded token for roles if there are roles and correct role move on
+    if (req.decodedJwt.roles && req.decodedJwt.roles.includes(role)) {
+      next();
+    } else {
+      res.status(403).json({ you: "you have no power here!" });
+    }
+  };
+}
+
+server.get("/api/users", restricted, checkRole("Admin"), (req, res) => {
   Users.find()
     .then(users => {
-      res.json(users);
+      res.json({ users, decodedToken: req.decodedJwt });
     })
     .catch(err => res.send(err));
 });
